2265876e0d95d6d813b99ce953398114a3710225
Andrii <andrejveremcuk712@gmail.com>
2025-01-09 21:58:49 +0200

    f fix

--- src/app/_components/lib/button.tsx
+++ src/app/_components/lib/button.tsx
@@ -5,11 +5,13 @@ type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
 
 export const Button = (props: ButtonProps) => {
   return (
-    <motion.button
+    <button
       className={twMerge(props.className, "rounded-lg border-2 px-5 py-3")}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.8 }}
-      {...props}
+     // whileHover={{ scale: 1.1 }}
+     // whileTap={{ scale: 0.8 }}
+      
+	  {...props}
+
     />
   );
 };
