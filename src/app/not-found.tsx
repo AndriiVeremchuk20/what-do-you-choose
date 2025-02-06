d7665df1e08788854271c845de13b4ea8a96ba3b
Andrii <andrejveremcuk712@gmail.com>
2025-02-06 21:54:56 +0200

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Error404 from "~/components/err-404";
+
+export default function NotFound() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+		<Error404/>
+	</main>
+  );	
+}
