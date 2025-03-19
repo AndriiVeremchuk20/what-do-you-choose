b695ba99a8a46d60f3d3e32193fb6a7123062310
Andrii <andrejveremcuk712@gmail.com>
2025-03-19 22:20:48 +0200

    feat: set gpt model

--- src/services/openai/index.ts
+++ src/services/openai/index.ts
@@ -8,11 +8,11 @@ const continueStory = async (storyText: string, messages: Message[]) => {
   const response = await OpenAIClient.chat.completions.create({
     model: "gpt-4o-2024-08-06",
     messages: [
-		{
-			role: "system",
-			content: "You're a generator of interesting stories"
-		},
-		{
+      {
+        role: "system",
+        content: "You're a generator of interesting stories",
+      },
+      {
         role: "user",
         content: `Continue story (30 - 50 words) (${storyText}) and propose user 2 ways to answer. language (ukr), use emoji`,
       },
